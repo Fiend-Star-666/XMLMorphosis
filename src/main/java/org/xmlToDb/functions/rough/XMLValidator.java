305e83e3a31955cd90e7d07678b5da3b2e785c5e
Gursimran <gurmsi1060@outlook.com>
2024-07-01 01:53:56 +0530

    adding type checking

---
+++ src/main/java/org/xmlToDb/functions/rough/XMLValidator.java
@@ -0,0 +1,41 @@
+package org.xmlToDb.functions.rough;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+
+public class XMLValidator {
+    public static void main(String[] args) {
+        String xmlPath = "src/main/resources/data/data.xml";
+        try {
+            String schemaPath = "src/main/resources/schemas/simple-schema.xsd";
+            xmlPath = "src/main/resources/data/data.xml";
+
+            // 1. Lookup a factory for the W3C XML Schema language
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+            // 2. Compile the schema.
+            File schemaLocation = new File(schemaPath);
+            Schema schema = factory.newSchema(schemaLocation);
+
+            // 3. Get a validator from the schema.
+            Validator validator = schema.newValidator();
+
+            // 4. Parse the document you want to check.
+            Source source = new StreamSource(new File(xmlPath));
+
+            // 5. Check the document
+            validator.validate(source);
+            System.out.println(xmlPath + " is valid.");
+
+        } catch (Exception ex) {
+            // The document is not valid
+            System.out.println(xmlPath + " is not valid because ");
+            System.out.println(ex.getMessage());
+        }
+    }
+}
